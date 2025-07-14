3851e7a45ce023b12f428baaebfd0b9062db1c95
Radoslaw Kaczybura <radoslaw.kaczybura@gmail.com>
2025-07-14 21:17:38 +0200

    Add user registration feature with password match check

---
+++ accounts/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+from django.contrib.auth.models import User
+
+class RegisterUserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput)
+    confirm_password = forms.CharField(widget=forms.PasswordInput)
+
+    def clean(self):
+        data = super().clean()
+        if data['password'] != data['confirm_password']:
+            raise forms.ValidationError("Passwords don't match")
+    class Meta:
+        model = User
+        fields = ('username', 'password', 'confirm_password')
+        help_texts = {
+            'username': ''
+        }
+
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from accounts.forms import RegisterUserForm
+from django.views import View
 
-# Create your views here.
+class RegisterView(View):
+    def get(self, request):
+        form = RegisterUserForm()
+        return render(request, 'form.html', {'form': form})
+
+    def post(self, request):
+        form = RegisterUserForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.set_password(form.cleaned_data['password'])
+            user.save()
+            return redirect('home')
+        return render(request, 'form.html', {'form': form})
